a945ab39aa77f25f72d881685089469733a7aeee
slenderock <sergiy20101995@ukr.net>
2016-05-20 16:50:47 +0300

    Merge remote-tracking branch 'origin/master'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    ssage aborts
    # the commit.

--- Task2.rb
+++ Task2.rb
@@ -1,8 +1,9 @@
+#v1.0 first version of program without classes
 some_hash = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30,
 			 key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
 i = 0
 some_hash.each_key  do |current|
-	#puts current.to_s.scan(/\byes\w*/)
+	#puts current.to_s.scan(/\byes\w*/) #need for testing
 	i += 1 if current.to_s.scan(/\byes\w*/).first
 end
 puts i
