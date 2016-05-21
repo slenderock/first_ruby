74c21b534dc7caaf4ea17c0a0eb6fc2506c63455
slenderock <sergiy20101995@ukr.net>
2016-05-22 02:41:35 +0300

    fix recursive method for symbolize key of hash

--- Task4.rb
+++ Task4.rb
@@ -5,26 +5,34 @@ class Parse
     @i = 0
   end
 
-  # def to_string
-  #   @some_hash.key = some_hash.key.to_s
-  #     next key
-  #     some_hash.to_string
-  # end
+  def symbolize (hash = @some_hash)
+    key    = hash.first[0]
+    value  = hash.first[1]
+    result = {}
 
-  def scan_yes
-    @some_hash.each_key  do |current|
-    #puts current.to_s.scan(/\byes\w*/) #need for testing
-    @i += 1 if current.to_s.scan(/\byes\w*/).first
-    i.get_resault
+    hash.delete(key)
+    result.merge!(symbolize(hash)) unless hash.empty?
+
+    result.merge(key.to_s.to_sym => value)
   end
 
+  def scan_hash_yes
+
+    @some_hash.each_key  do |current|
+      #puts current.to_s.scan(/\byes\w*/) #need for testing
+      @i += 1 if current.to_s.scan(/\byes\w*/).first
+    end
+
+    get_resault
   end
 
   def get_resault
-    puts @i
+    puts ("result = #{@i}")
   end
 end
 
-hsh = Parse.new ['yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: 30,
-  key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014]
-hsh.scan_yes
+hsh = Parse.new ({'yes' => 23, 'b'=> 'travel', 'yesterday' => 34, 5 => '234',
+                  :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34,
+                   'yesteryear' => 2014})
+puts hsh.symbolize
+hsh.scan_hash_yes
