d237bb82f66f85be6497ffa86e9d8f461095dcc3
slenderock <sergiy20101995@ukr.net>
2016-05-22 19:33:47 +0300

    add method Split string to array

--- Task5.rb
+++ Task5.rb
@@ -1,10 +1,23 @@
 class String
-  def string_to_array (some_str)
-    some_str.split('::')
-    some_str.split('|')
+  def string_split_to_array (some_str)
+    some_str = some_str.split(@spliter = '|')  if some_str.class == String and some_str.to_s.scan(/[{|}]/)  != []
+    some_str = some_str.split(@spliter = '::') if some_str.class == String and some_str.to_s.scan(/[{::}]/) != []
+    form_for_output(some_str)
+  end
 
+  def form_for_output (some_arr)
+    result = Array.new(some_arr.length)
+    result.each_index { |current|
+
+      result[current] = some_arr.join(@spliter)
+      some_arr.pop
+    }
+    result
   end
 end
 
 str1 = String.new
-puts str1.string_to_array ("Fizz|Buzz|Wizz")
+p str1.string_split_to_array ("Fizz|Buzz|Wizz")
+
+str2 = String.new
+p str2.string_split_to_array ("Fizz::Buzz::Wizz")
