20a184723f5629b05a16c209f1eb04738d09d36c
slenderock <sergiy20101995@ukr.net>
2016-05-22 21:07:54 +0300

    add regular expression for scan string

--- Task5.rb
+++ Task5.rb
@@ -1,7 +1,7 @@
 class String
   def string_split_to_array (some_str)
-    some_str = some_str.split(@spliter = '|')  if some_str.class == String and some_str.to_s.scan(/[{|}]/)  != []
-    some_str = some_str.split(@spliter = '::') if some_str.class == String and some_str.to_s.scan(/[{::}]/) != []
+    some_str = some_str.split(/[:]{2}|[({|})]/)
+
     form_for_output(some_str)
   end
 
@@ -9,7 +9,7 @@ class String
     result = Array.new(some_arr.length)
     result.each_index { |current|
 
-      result[current] = some_arr.join(@spliter)
+      result[current] = some_arr.join("::")
       some_arr.pop
     }
     result
@@ -17,7 +17,7 @@ class String
 end
 
 str1 = String.new
-p str1.string_split_to_array ("Fizz|Buzz|Wizz")
+p str1.string_split_to_array ("Fizz::Buzz|Wizz")
 
 str2 = String.new
-p str2.string_split_to_array ("Fizz::Buzz::Wizz")
+p str2.string_split_to_array ("Fizz|Buzz::Wizz")
