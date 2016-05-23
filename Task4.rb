54474c3e1441234ef71c5968d4d5a6d0a8c75ecd
slenderock <sergiy20101995@ukr.net>
2016-05-23 04:12:38 +0300

    add comments

--- Task4.rb
+++ Task4.rb
@@ -2,37 +2,38 @@
 class Parse
   def initialize (some_hash)
     @some_hash = some_hash
-    @i = 0
   end
 
-  def symbolize (hash = @some_hash)
+  def symbolize (hash = @some_hash)                        #recursive method for converting to symbol each key
     key    = hash.first[0]
     value  = hash.first[1]
     result = {}
 
-    hash.delete(key)
-    result.merge!(symbolize(hash)) unless hash.empty?
+    hash.delete(key)                                       #destroy hash
+    result.merge!(symbolize(hash)) unless hash.empty?      #set all value and keys from destroyed hash to new result hash
 
-    result.merge(key.to_s.to_sym => value)
+    result.merge(key.to_s.to_sym => value)                 #return current symbolized key
   end
 
-  def scan_hash_yes
+  def hash_scan_for_the_yes
+    i = 0
 
     @some_hash.each_key  do |current|
-      #puts current.to_s.scan(/\byes\w*/) #need for testing
-      @i += 1 if current.to_s.scan(/\byes\w*/).first
+      #puts current.to_s.scan(/\byes\w*/)                  #need for testing
+      i += 1 if current.to_s.scan(/\byes\w*/).first
     end
 
-    get_resault
+    get_resault(i)
   end
-
-  def get_resault
-    puts ("result = #{@i}")
+                                                           #method for output as says in task
+  def get_resault(i)
+    p ("result = #{i}")
   end
 end
 
 hsh = Parse.new ({'yes' => 23, 'b'=> 'travel', 'yesterday' => 34, 5 => '234',
                   :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34,
                    'yesteryear' => 2014})
-puts hsh.symbolize
-hsh.scan_hash_yes
+
+hsh.hash_scan_for_the_yes
+p hsh.symbolize
