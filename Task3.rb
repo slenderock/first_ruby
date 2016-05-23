ad594d86133c0d01923d91e151a5d17adbcfcdf1
slenderock <sergiy20101995@ukr.net>
2016-05-23 03:46:31 +0300

    fix an code format mistake

--- Task3.rb
+++ Task3.rb
@@ -7,29 +7,30 @@ class Sum_of_neg
   end
 
   def randomize
-    table.each_index {|i|
-      table[i].each_index {|j|
+    table.each_index do |i|
+      table[i].each_index do |j|
         table[i][j] = rand(-10...10)
-      }
-    }
+      end
+    end
   end
 
   def calculate
     puts ('Negative elements:')
     res = 0
-    table.each_index {|i|
-      table[i].each_index {|j|
+    table.each_index do |i|
+      table[i].each_index do |j|
         if table[i][j] < 0
           puts table[i][j]
           res +=  table[i][j]
         end
-      }
-    }
+      end
+    end
 
-    puts ("The sum of negative elements is #@res")
+    p 'The sum of negative elements :'
+    res
   end
 end
 
 t = Sum_of_neg.new([Array.new(4), Array.new(4), Array.new(4), Array.new(4)])
 t.randomize
-t.calculate
+p t.calculate
