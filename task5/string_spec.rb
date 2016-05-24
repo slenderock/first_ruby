9dc72f7a3310c48435692dbe589ec40f738cb479
slenderock <sergiy20101995@ukr.net>
2016-05-25 01:23:49 +0300

    add spec test that cheh right answer

--- task5/string_spec.rb
+++ task5/string_spec.rb
@@ -1,1 +1,11 @@
 require "minitest/autorun"
+
+describe String do
+  require "./string"
+
+    describe "string_split_to_array" do
+      it "right answer" do
+         "Fizz::Buzz::Wizz".string_split_to_array.must_equal ["Fizz::Buzz::Wizz", "Fizz::Buzz", "Fizz"]
+    end
+  end
+end
