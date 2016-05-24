abbdf95de4747426b9ee7922d555ea3a7a378c34
slenderock <sergiy20101995@ukr.net>
2016-05-24 16:46:13 +0300

    add select and inject method

--- Task1.rb
+++ Task1.rb
@@ -1,5 +1,4 @@
 # first version of program without classes
 table = Array.new(4) { Array.new(4) {rand(-10...10)}}
-p table.flatten.count {|a| a < 0}
-
+p table.flatten.select {|a| a < 0}.inject(0, :+)
 
