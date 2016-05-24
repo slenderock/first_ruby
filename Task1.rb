c0d828fbdd529d1dcf32536d90582afb6762c12a
slenderock <sergiy20101995@ukr.net>
2016-05-24 16:11:38 +0300

    optimize code

--- Task1.rb
+++ Task1.rb
@@ -1,18 +1,5 @@
 # first version of program without classes
-table = [Array.new(4), Array.new(4), Array.new(4), Array.new(4)]
-sum_of_neg = 0
+table = Array.new(4) { Array.new(4) {rand(-10...10)}}
+p table.flatten.count {|a| a < 0}
 
-puts ('Negative elements:')
 
-table.each_index do |i|
-  table[i].each_index do |j|
-    table[i][j] = rand(-10...10)
-    if table[i][j]<0
-    	puts table[i][j]
-    	sum_of_neg +=  table[i][j]
-    end
-  end
-end
-
-puts ('The sum of negative elements is')
-puts sum_of_neg
