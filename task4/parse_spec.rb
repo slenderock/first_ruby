a7ff8451e4dc9beff7d3ca95be14b94b34b9f3c6
slenderock <sergiy20101995@ukr.net>
2016-05-25 15:36:59 +0300

    small code design changes

--- task4/parse_spec.rb
+++ task4/parse_spec.rb
@@ -4,20 +4,20 @@ describe Hash do
   require "./parse"
 
     before do
-      @hash = Parse.new ({'yes' => 23, 'b'=> 'travel', 'yesterday' => 34, 5 => '234',
-                  :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34,
-                   'yesteryear' => 2014})
+      @hash = Parse.new ({'yes' => 23, 'b'=> 'travel', 'yesterday' => 34,\
+                            5 => '234', :yesss => :fg, try: 30,\
+                          key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
     end
 
-    describe "symbolize" do
-      it "right answer" do
-         @hash.symbolize.keys.each{|current| current.is_a? Symbol }
+    describe "Need convert each key type to Symbol" do
+      it "successful symbolizing" do
+         @hash.symbolize.keys.each{ |current| current.is_a? Symbol}
     end
 
-    # describe "hash_scan_for_the_yes" do
-      # it "right answer" do
-         # .hash_scan_for_the_yes.must_equal
-    # end
 
+    describe "hash_scan_for_the_yes" do
+      # it "successful scaning" do
+         # @hash.hash_scan_for_the_yes
+    end
   end
 end
