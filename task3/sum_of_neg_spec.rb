ff2530551d42161c1740eba7cc9674bee9b3be0c
slenderock <sergiy20101995@ukr.net>
2016-05-25 15:54:31 +0300

    change range for testing

--- task3/sum_of_neg_spec.rb
+++ task3/sum_of_neg_spec.rb
@@ -3,12 +3,12 @@ require "minitest/autorun"
 describe Array do
   require "./sum_of_neg"
     before do
-       @arr = SumOfNeg.new(4,4)
+      @arr = SumOfNeg.new(100,100)
     end
 
     describe "calculate" do
       it "right answer type" do
         @arr.calculate.is_a? Fixnum
-    end
+      end
   end
 end
