a74f0a6a7425004a45225682f218684b985039e1
slenderock <sergiy20101995@ukr.net>
2016-05-26 22:10:09 +0300

    try push after reinstalling system

--- task4/parse.rb
+++ task4/parse.rb
@@ -19,6 +19,7 @@ class Parse
   end
 end
 
+
 # hsh = Parse.new ({'yes' => 23, 'b'=> 'travel', 'yesterday' => 34,\
                       # 5 => '234', :yesss => :fg, try: 30,\
                       # key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
