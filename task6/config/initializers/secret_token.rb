ca062481f668baee86549d7dd0b557b2cd4d9528
slenderock <sergiy20101995@ukr.net>
2016-05-30 22:14:41 +0300

    delete task6

--- task6/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Task6::Application.config.secret_token = '741b93e86e52e16ee245db44497a6cc6a29619b708b73436e4bcdbb1c0fcb5619d2916625f5db271792d3676e301bdbf6608ccb5a29864875999876531d63936'
